9f45d2bf7e170a23e443873901a665e41f6fad7c
STRO09 <sagarjanjoted123@gmail.com>
2025-05-04 12:08:02 +0530

    Add GitHub Actions deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Required for static export
-  assetPrefix: isProd ? '/my-next-app/' : '', // <-- Replace with your repo name
+  assetPrefix: isProd ? '/' : '', // <-- Replace with your repo name
   trailingSlash: true, // Optional but recommended for static export
   typescript: {
     ignoreBuildErrors: true,
