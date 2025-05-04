a99b57c498ae7d5f81c3633b5cdad06d14982d72
STRO09 <sagarjanjoted123@gmail.com>
2025-05-04 12:03:47 +0530

    removed idx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type {NextConfig} from 'next';
 
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Required for static export
+  assetPrefix: isProd ? '/my-next-app/' : '', // <-- Replace with your repo name
+  trailingSlash: true, // Optional but recommended for static export
   typescript: {
     ignoreBuildErrors: true,
   },
